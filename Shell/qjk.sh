b0aa4f048867512fcc9d04c501ac8e4627d0ab96
wan230114 <1170101471@qq.com>
2019-09-18 10:33:40 +0800

    2019-09-18  10:33:31

--- Shell/qjk.sh
+++ Shell/qjk.sh
@@ -1,9 +1,10 @@
-path=`pwd` && mkdir ~/tmplog 2>/dev/null
+path=`pwd`
+mkdir ~/tmplog 2>/dev/null
 
 # 决定是在当前目录下执行还是 ~/tmplog 下，第三个参数不为空则在当前目录下
 if [ "$3" ]; then sleep 0; else cd ~/tmplog; fi
 
-if [ "`ls /ifs/TJPROJ3/Plant/chenjun/mytools/Shell 2>/dev/null`" ]
+if [ "`ls /ifs/TJPROJ3/Plant/chenjun/mytools/Shell &>/dev/null`" ]
 then
     nohup python /ifs/TJPROJ3/Plant/chenjun/mytools/tools_jiqun/moni_renwu.py $1 $2 >nohup-moni_renwu.py.o && echo &&echo "监控完成！" && cat nohup-moni_renwu.py.o &
 else
