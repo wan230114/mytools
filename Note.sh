608258946d2c1d49024970a6e9eeec9211fe4bdd
wan230114 <1170101471@qq.com>
2019-09-21 14:17:39 +0800

    2019-09-21  14:17:37

--- Note.sh
+++ Note.sh
@@ -1,19 +1,6 @@
 # python 管理
 pip3 install -i  https://pypi.tuna.tsinghua.edu.cn/simple/  
 
-
-############################################################
-######################## 软件记录 ##########################
-############################################################
-### 软件语法 ###
-# blast语法
-blastall -d $db -i $qu  -m 8  -F F -p blastn
-
-### 软件脚本 ###
-# 画图
-alias circos="/PUBLIC/software/public/System/Perl-5.18.2/bin/perl /PUBLIC/software/public/Graphics/circos-0.64/bin/circos "
-
-
 ############################################################
 ######################## 集群管理 ##########################
 ############################################################
@@ -25,11 +12,46 @@ perl /home/liuwenbin/STAT/CheckrmScript/CheckrmScript.v2.pl  --group 1908,1919,1
 
 ### 上传阿里云冷存储 ###
 # 身份验证配置文件，用于-c后面
-/NJPROJ2/Plant/Projects/Plant2/zhangkaijian/program/Aliyun/bin/general_ossutilconfig
 /TJPROJ1/DENOVO/PROJECT/zhangkaijian/program/Aliyun/bin/general_ossutilconfig
+/NJPROJ2/Plant/Projects/Plant2/zhangkaijian/program/Aliyun/bin/general_ossutilconfig
 # 天津上传or下载（原始脚本）
 ...
 # 南京上传or下载（原始脚本）
-/NJPROJ2/Plant/zhangwenlin/Cluster_management/Aliyun/xiazai/bin/ossutil64 -c /NJPROJ2/Plant/zhangwenlin/Cluster_management/Aliyun/xiazai/bin/ossutilconfig cp -r /NJPROJ2/Plant/chenjun/prj/01.WGS/X101SC19010440-Z01_184caomei/00.data-all-rawdata/wenku/01.0226.rawdata/BDSW190002954-1a/BDSW190002954-1a_L4_1.fq.gz oss://plant-nj/Plant_Group2/X101SC19010440-Z01_184caomei/00.data-all-rawdata/wenku/01.0226.rawdata/BDSW190002954-1a/BDSW190002954-1a_L4_1.fq.gz
+/NJPROJ2/Plant/zhangwenlin/Cluster_management/Aliyun/xiazai/bin/ossutil64 -c /NJPROJ2/Plant/Projects/Plant2/zhangkaijian/program/Aliyun/bin/general_ossutilconfig cp -r /NJPROJ2/Plant/chenjun/prj/01.WGS/X101SC19010440-Z01_184caomei/00.data-all-rawdata/wenku/01.0226.rawdata/BDSW190002954-1a/BDSW190002954-1a_L4_1.fq.gz oss://plant-nj/Plant_Group2/X101SC19010440-Z01_184caomei/00.data-all-rawdata/wenku/01.0226.rawdata/BDSW190002954-1a/BDSW190002954-1a_L4_1.fq.gz
 # 现成封装开发脚本，可以直接运行，会自动投递
 /NJPROJ1/PAG/Plant/Projects/Business/GWAS/WGS/P101SC17071673-01_300_yama/10.release/work_aliyun_Data.sh
+
+
+############################################################
+######################## 软件记录 ##########################
+############################################################
+### 软件语法 ###
+# blast语法
+blastall -d $db -i $qu  -m 8  -F F -p blastn
+
+### 软件脚本 ###
+# 画circos图：
+alias circos="/PUBLIC/software/public/System/Perl-5.18.2/bin/perl /PUBLIC/software/public/Graphics/circos-0.64/bin/circos "
+
+
+# 软件建立索引：
+ref=App.fa
+#建立bwa比对所需要的index	#超过1G的用这个
+#/PUBLIC/software/public/Alignment/bwa/bwa-0.7.8/bwa index -a bwtsw $ref
+#建立bwa比对所需要的index	#小于1G的用这个
+#/NJPROJ2/Crop/share/pipeline/current/BSA/00.bin/indBin/bwa index -a is $ref
+#建立samtools检测SNP所需要的index
+#/NJPROJ2/Crop/share/pipeline/current/BSA/00.bin/indBin/samtools  faidx $ref
+#建立dist文件
+#/NJPROJ2/Crop/share/software/picard-tools-1.96/CreateSequenceDictionary.jar R=$ref O=ref.dict TMP_DIR=/NJPROJ2/Crop/share/pipeline/current/BSA/00.bin
+#soap
+/NJPROJ1/PAG/Crop/share/software/appLinkPath/bowtie-1.0.1 $ref
+#bwa
+/NJPROJ1/PAG/Crop/share/software/appLinkPath/bwa index -a bwtsw $ref
+#sam
+/NJPROJ1/PAG/Crop/share/software/appLinkPath/samtools faidx $ref
+#base_stat
+/NJPROJ1/PAG/Plant/Projects/Business/Variation/WGS/P101SC17091285-01_tomato/sample_26/00.ref/base-stat.pl $ref > $ref.base.stat.xls
+#建立dist文件
+/NJPROJ2/Crop/share/software/picard-tools-1.96/CreateSequenceDictionary.jar R=$ref O=./App
+
