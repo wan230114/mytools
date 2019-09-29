78e27450a2341d5fbbf175296c684ef7728452e0
wan230114 <1170101471@qq.com>
2019-09-29 09:48:18 +0800

    2019-09-29  09:48:17

---
+++ print_source.sh
@@ -0,0 +1,4 @@
+tools_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+echo source ${tools_path}/bashrc_my.cfg
+echo mymail=\"1170101471@qq.com\"
+echo source ${tools_path}/Tools_bashrc.sh
