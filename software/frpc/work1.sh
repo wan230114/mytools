7133b7cb9008c6535785a804e4850933ef2d7849
wan230114 <1170101471@qq.com>
2019-11-09 02:10:04 +0800

    2019-11-09  02:10:04

---
+++ software/frpc/work1.sh
@@ -0,0 +1,6 @@
+set -e
+ps xjf |grep "./test -c cfg"|grep -v grep|awk '{print $2}' |xargs kill
+sudo /etc/init.d/ssh start
+nohup ./frpc -c frpc.ini &>>log &
+#nohup sleep 86400 && ps xjf |grep "./test -c cfg"|grep -v grep|awk '{print $2}' |xargs kill &
+
