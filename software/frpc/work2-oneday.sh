0b70ea2a65a9cbdd7604bf5b80e444664e484227
wan230114 <1170101471@qq.com>
2019-11-25 15:19:49 +0800

    2019-11-25  15:19:49

--- software/frpc/work2-oneday.sh
+++ software/frpc/work2-oneday.sh
@@ -1,5 +1,5 @@
 set -e
-ps xjf |grep "./test -c cfg"|grep -v grep|awk '{print $2}' |xargs kill
+ps xjf |grep "./test -c cfg"|grep -v grep|awk '{print $2}' |xargs -i kill {}
 sudo /etc/init.d/ssh start
 nohup ./frpc -c frpc.ini &>>log &
 nohup sleep 86400 && ps xjf |grep "./test -c cfg"|grep -v grep|awk '{print $2}' |xargs kill &
