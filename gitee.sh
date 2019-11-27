71e176622ecdb6895f65db66fd9e2a4029834455
wan230114 <1170101471@qq.com>
2019-11-27 23:29:57 +0800

    2019-11-27  23:29:57 gitee_update

--- gitee.sh
+++ gitee.sh
@@ -1,9 +1,12 @@
 # https://gitee.com/wan230114/mytools.git
-echo 拉取中...
+echo "***********"
+echo ">拉取中..."
 sh Get_gitee.sh
 
 echo
-echo 上传中...
+echo "***********"
+echo ">上传中..."
+read -p "Input this commit: " val echo $val
 git add -A
-git commit -a -m "`date "+%F  %H:%M:%S"` $1"
+git commit -a -m "`date "+%F  %H:%M:%S"` $val"
 git push -u origin master        # 上传仓库到码云
