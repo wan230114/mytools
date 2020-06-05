280ef470fd7a7cb92eec1b440d3991f3c281cb74
wan230114 <1170101471@qq.com>
2020-06-05 11:15:32 +0800

    :sparkles: Minor update

--- bashrc_Tools.sh
+++ bashrc_Tools.sh
@@ -76,11 +76,12 @@ alias ca="${tools_path}/Shell/common/ccat "
 
 ###  路径管理工具  ###
 # 用于返回当前文件(夹)绝对路径
-pwdfile(){
+#pwdfile(){
 #echo $@|sed "s# #\n#g"|xargs -i echo `pwd`/{}
-echo $@|sed "s# #\n#g"|while read x; do echo $(cd $x; pwd); done
-}
-alias f=pwdfile  # 用于返回当前文件夹某文件或目录的路径, f [file/dir] [file/dir]...
+##echo $@|sed "s# #\n#g"|while read x; do echo $(cd $x; pwd); done
+#}
+#alias f=pwdfile  # 用于返回当前文件夹某文件或目录的路径, f [file/dir] [file/dir]...
+alias f="realpath "
 
 ###  文件磁盘管理工具  ###
 # asum统计某一列的和，用法示例: `asum 1 file`
