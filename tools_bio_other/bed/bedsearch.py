09add2deb622d828e111201838c11c791b9b43a3
wan230114 <1170101471@qq.com>
2020-11-04 13:05:15 +0800

    🐛  BUG update for bedsearch.

--- tools_bio_other/bed/bedsearch.py
+++ tools_bio_other/bed/bedsearch.py
@@ -17,24 +17,30 @@ import sys
 import argparse
 
 
+class InputFileERRO(BaseException):
+    pass
+
+
 def fargv():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=('bed区间搜索，输入bed文件为4列，第4列为基因名\n'
                      '输出为：\n'
                      'chr  start_ref  end_ref  gene_ref  '
-                     'start_tar  end_tar  gene_tar  dis'),
+                     'start_tar  end_tar  gene_tar  dis  type\n'),
         epilog=('注意事项：\n'
                 '    None\n'
                 ))
     parser.add_argument('inputfile', type=str,
-                        help=('输入文件'))
+                        help=('输入文件，要求和ref输入文件的区间最多一个重叠'))
     parser.add_argument('-r', '--ref', type=str, required=True,
                         help=('输入文件'))
     parser.add_argument('-o', '--outfile', type=str, default=False,
                         help=('输出文件，省略参数默认为stdout'))
     parser.add_argument('-s', '--spacing', type=str, default="3000,3000",
                         help=('上下游搜索长度，默认为 “3000,3000”'))
+    parser.add_argument('-pa', '--print_all', action='store_true',
+                        help='是否打印全部inbed内容，未查找到的，用 “.” 补齐')
     args = parser.parse_args()
     return args.__dict__
 
@@ -52,68 +58,122 @@ def deal_ref(ref):
 
 
 def comp(D, CHR, START, END, spacing):
-    seed = [0] + [x[0] for x in D[CHR]]
-    s, e = 0, len(seed) - 1
-    # 1) 查找起始位置
+    SEED = [0] + [x[0] for x in D[CHR]]
+    LEN_SEED = len(SEED) - 1
+    s, e = 0, LEN_SEED
     while True:
-        if s == e or seed[s] == START or seed[e] == START:
+        # 1) 二分
+        n = int((e - s)/2)
+        n = 1 if not n else n
+        # 2) 重新分配坐标
+        if START < SEED[s+n]:
+            e = e - n
+        else:
+            s = s + n
+        # 3）判断退出条件
+        if s == e or SEED[s] == START or SEED[e] == START:
             break
-        if START > seed[s]:
-            if START < seed[e]:
-                e = int((e - s)/2)
+    s = 0 if s == 0 else s - 1  # 归位，因为前面加了1，此处index归位
+    dis = 0
+    # 处理第一个
+    if s == 0 and START < D[CHR][s][0]:
+        if END < D[CHR][s][0]:
+            dis = D[CHR][s][0] - END
+            res = D[CHR][s], (dis, "Right")
+        else:
+            res = D[CHR][s], (dis, "Intersect_right")
+            # if END <= D[CHR][s][1]:
+            #     raise(InputFileERRO("相交区间跨越多个参考系，请检查输入文件的合理性，出错行：\n"
+            #                     "%s\t%s\t%s" % (CHR, START, END)))
+    # 处理位于最后一块区间右边
+    elif s == len(D[CHR]) - 1:
+        if START > D[CHR][s][1]:
+            dis = D[CHR][s][1] - START
+            res = D[CHR][s], (dis, "Left")
+        elif START >= D[CHR][s][0]:
+            res = D[CHR][s], (dis, "Intersect_left")
+    # 2) 判断区间
+    elif START <= D[CHR][s][1]:
+        if END <= D[CHR][s][1]:
+            res = D[CHR][s], (dis, "Intersect")
+        else:
+            res = D[CHR][s], (dis, "Intersect_left")
+            # if END <= D[CHR][s+1][0]:
+            #     raise(InputFileERRO("相交区间跨越多个参考系，请检查输入文件的合理性，出错行：\n"
+            #                         "%s\t%s\t%s" % (CHR, START, END)))
+    elif START > D[CHR][s][1]:
+        if END < D[CHR][s+1][0]:
+            # 右侧区间
+            # 3) 判断区间距离
+            dis_l = START - D[CHR][s][1]
+            dis_r = D[CHR][s+1][0] - END
+            if dis_l < dis_r:
+                dis = -dis_l
+                res = D[CHR][s], (dis, "Left")
+            elif dis_l > dis_r:
+                dis = dis_r
+                res = D[CHR][s+1], (dis, "Right")
             else:
-                s = e
-    s -= 1  # 归位，因为前面加了1，此处index归位
-    # 2) 判断是否在区间内
-    if END < D[CHR][s][1]:
-        res = D[CHR][s], 0
-    # 3) 判断区间距离
-    dis_l = START - D[CHR][s][1]
-    dis_r = D[CHR][s+1][0] - END
-    if dis_l <= dis_r:
-        res = D[CHR][s], dis_l
-    else:
-        res = D[CHR][s+1], -dis_r
+                dis = dis_l
+                res = (('%s,%s' % (D[CHR][s][0], D[CHR][s+1][0]),
+                        '%s,%s' % (D[CHR][s][1], D[CHR][s+1][1]),
+                        '%s,%s' % (D[CHR][s][2], D[CHR][s+1][2]),
+                        ), ('%s,%s' % (-dis, dis), "Left,Right")
+                       )
+        else:
+            res = D[CHR][s+1], (dis, "Intersect_right")
+            # if END <= D[CHR][s+1][1]:
+            #     raise(InputFileERRO("相交区间跨越多个参考系，请检查输入文件的合理性，出错行：\n"
+            #                         "%s\t%s\t%s" % (CHR, START, END)))
+    # print(START, END)
+    # from pprint import pprint
+    # pprint(D[CHR][s-1:s+10])
+    # print(s, e, res)
+    # sys.exit()
     # 看是否在指定距离内，如果不在，则不进行查找
-    # print(CHR, START, END, res[-1])
-    if (res[-1] > 0 and res[-1] < spacing[0]) or \
-            (res[-1] < 0 and -res[-1] < spacing[1]):
+    if dis == 0 or \
+        (dis > 0 and dis < spacing[0]) or \
+            (dis < 0 and -dis < spacing[1]):
         return res
     else:
         return None
 
 
-def fmain(inputfile, ref, outfile, spacing):
+def fmain(inputfile, ref, outfile, spacing, print_all):
     try:
         spacing = spacing.split(',')
         spacing = int(spacing[0]), int(spacing[1])
         if spacing[0] <= 0 or spacing[1] <= 0:
-            print("Error，上下游数值请输入大于零的数", file=sys.stderr)
-            raise
+            raise(InputFileERRO("Error，上下游数值请输入大于零的数"))
     except:
         print(" -s 输入参数有误", file=sys.stderr)
         raise
     fo = open(outfile, 'w') if outfile else sys.stdout
-
     D_ref = deal_ref(ref)
     try:
         with open(inputfile) as fi:
             for line in fi:
                 line = line.strip()
                 if line:
-                    x = line.split("\t")
+                    x = line.split("\t")[0:4]
                     CHR, START, END, NAME = x[0], int(x[1]), int(x[2]), x[3]
                     nearinfo = comp(D_ref, CHR, START, END, spacing)
                     if nearinfo:
-                        print(CHR, *nearinfo[0], *x,
-                              nearinfo[1], sep="\t", file=fo)
+                        print(*x,
+                              CHR, *nearinfo[0],
+                              *nearinfo[1],
+                              sep="\t", file=fo)
+                    elif print_all:
+                        print(*x, ('\t.'*5)[1:])
     except IndexError:
-        print("\n---\nErro, 请检查输入文件格式是否正确", file=sys.stderr)
-        raise
+        raise(InputFileERRO("\n---\nErro, 请检查输入文件格式是否正确"))
 
 
 def main():
-    # sys.argv = '.py -r bedsearch.py--test/ref.bed bedsearch.py--test/in.bed'.split()
+    # sys.argv = '.py -pa -s 100000,100000 -r bedsearch.py--test/ref.bed bedsearch.py--test/in.bed'.split()
+    # sys.argv = ('.py -s 100000,100000 '
+    #             '-r protein_coding.v32.position.bed'
+    #             ' in2.bed').split()
     args = fargv()
     # print(*list(args.keys()), sep=", ")
     # print(*list(args.values()), sep=", ")
---
+++ tools_bio_other/bed/bedsearch.py--test/run.sh
@@ -0,0 +1,2 @@
+python3 ../bedsearch.py -r ref.bed in.bed
+python3 ../bedsearch.py -r protein_coding.v32.position.bed in2.bed
